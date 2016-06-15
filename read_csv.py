3312923af8602f586837d225c9bed293b4df759d
pernambucano <demelofernandes.paulo@gmail.com>
2016-06-14 22:19:49 -0300

    ajeitando erros de grafo e criando metodos para facilitar o uso

--- read_csv.py
+++ read_csv.py
@@ -12,16 +12,20 @@ theDict = {}
 def getting_users():
     users = []
     mDictPart = []
-    counter = 0
+
     with open('contra_e_a_favor_dilma.csv', 'r') as f, open('retweets.json', 'w') as cd:
         reader = csv.reader(f)
 
+        counter_rows = 0
+        counter_positions = 0
         for row in reader:
-            if counter >= 234:
+            # TODO contar a quantidade de rows no reader e não a quantidade de pessoas cadastradas
+            if counter_rows > 234:
                 group = 2
             else:
                 group = 1
-            
+
+            counter_rows += 1
             first_item = row[0]
             second_item = row[1]
             if first_item not in users:
@@ -30,19 +34,21 @@ def getting_users():
                 intDict["name"] = first_item
                 intDict["group"] = group
                 users.append(first_item)
-                intDict["posicao"] = counter
+                intDict["posicao"] = counter_positions
                 mDictPart.append(intDict)
-                counter += 1
+                counter_positions += 1
 
-            elif second_item not in users:
+
+            if second_item not in users:
 
                 intDict = {}
                 intDict["name"] = second_item
                 intDict["group"] = group
-                intDict["posicao"] = counter
+                intDict["posicao"] = counter_positions
                 users.append(second_item)
                 mDictPart.append(intDict)
-                counter += 1
+                counter_positions += 1
+
 
 
         pp = pprint.PrettyPrinter(indent=4)
@@ -51,7 +57,7 @@ def getting_users():
         myDict["nodes"] = mDictPart
         # pp.pprint(myDict)
         cd.write(dumps(myDict, cd, indent=4))
-        
+
         return users, myDict
 
 
@@ -64,7 +70,6 @@ def replacing_users_with_location(myDict):
 
 
         for d in reader:
-                
             for row in myDict["nodes"]:
                 if row["name"] == d[0] or row["name"] == d[1]:
                     index = d.index(row["name"])
@@ -78,14 +83,14 @@ def replacing_users_with_location(myDict):
 
 
         for d in reader:
-                
+
             for row in myDict["nodes"]:
                 if row["name"] == d[0] or row["name"] == d[1]:
                     index = d.index(row["name"])
                     d[index] = row["posicao"]
                     newrow = d
                     writer.writerow(newrow)
-                    
+
 
 
 def create_json_links():
@@ -112,20 +117,36 @@ def count_repetition():
     with open('output_file2.csv', 'r') as input_data, open('result_wo_rep.csv', 'w') as output_data:
         reader = csv.reader(input_data)
         writer = csv.writer(output_data)
-    
+
         seen = set() # set for fast O(1) amortized lookup
         for line in input_data:
             if line in seen: continue # skip duplicate
 
             seen.add(line)
             output_data.write(line)
-            
-        
-    
-    
+
+
+def make_final_json():
+    with open('links.json', 'r') as links, open('retweets.json', 'r') as retweets:
+        data_retweets = load(retweets)
+        data_links = load(links)
+
+    data_retweets.update(data_links)
+
+    with open('retweets.json', 'w') as f:
+        dumps(data_retweets, f)
+        f.write(dumps(data_retweets, f, indent=4))
+
+
+
+
+
+
+
 users, mDict = getting_users()
 replacing_users_with_location(mDict)
 create_json_links()
+make_final_json()
 # count_repetition()
 
 
